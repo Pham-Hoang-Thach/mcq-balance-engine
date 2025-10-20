a66184ac3b3a4eff701ce67fb014304ddbc4da61
Pham Hoang Thach <phamhoangthach@gmail.com>
2025-10-20 11:56:59 +0700

    Add mcq-balance-engine.el

---
+++ mcq-balance-engine.el
@@ -0,0 +1,9 @@
+;;; mcq-balance-engine.el --- aggregate loader  -*- lexical-binding: t; -*-
+
+(require 'mcq-persistent-org-combo-shuffler)
+(require 'mcq-extract-correct-answers)
+(require 'mcq-export-cleaned-questions)
+(require 'mcq-check-missing-correct-answers)
+
+(provide 'mcq-balance-engine)
+;;; mcq-balance-engine.el ends here
